6ce4ed75bc4dc34dd141f75b91548ca6d1bdc07f
Sahana Krishnaraj <143361100+sahana-krishnaraj@users.noreply.github.com>
2026-02-02 18:55:35 -0500

    Changed and moved a couple of files around

--- Python Scripts/app.py
+++ Python Scripts/app.py
@@ -1,15 +1,12 @@
 import streamlit as st
 import pandas as pd
 import function as fn 
-import plotly.express as px
+
 
 st.title("Data Center Siting Optimizer Prototype") 
 st.text("Recommends optimal locations to build data centers based on environmental risk factors.")
-
 WFH_value = st.slider("Select a weight for Wildfire Hazard Potential", 0.0, 1.0, 0.5)
 DR_value = st.slider("Select a weight for Drought Risk", 0.0, 1.0, 0.5)
 rank_value = st.slider("Select number of locations to recommend", 1, 20, 5)
 st.header("Top " + str(rank_value) + " Recommended Counties:")
 st.write(fn.environmental_score(WFH_value, DR_value, rank_value, high=True))
-
-
