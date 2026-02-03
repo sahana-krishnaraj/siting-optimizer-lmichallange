86aca2317317a2f50e15a6ee416b335c28f58128
Sahana Krishnaraj <143361100+sahana-krishnaraj@users.noreply.github.com>
2026-02-02 19:16:22 -0500

    ok

--- Python Scripts/app.py
+++ Python Scripts/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import MinMaxScaler
 
 #load the dataset we will use
 combined = pd.read_csv("/Users/sahana_krishnaraj/Github/siting-optimizer-lmichallange/Datasets/Cleaned Data/final_combined_data.csv")
@@ -13,7 +13,7 @@ subset = subset.copy()
 subset = subset.rename(columns={"H + VH Pct": "Wildfire Hazard Potential"})
 
 ###SHOULD I NORMALIZE THE SCORES BEFORE COMBINING?###
-scaler = MinMaxScaler()
+#scaler = MinMaxScaler()
 subset["Drought Risk Score"] = 0.0*subset["None"] + 0.1*subset["D0"] + 0.3*subset["D1"] + 0.4*subset["D2"] + 0.6*subset["D3"] + 0.9*subset["D4"]
 
 def environmental_score(wildfire_user, drought_user, rank, high=True):
@@ -27,7 +27,7 @@ def environmental_score(wildfire_user, drought_user, rank, high=True):
                                    drought_weight * subset["Drought Risk Score"])
     
     #standardize the Suitability Score
-    subset["Suitability Score"] = scaler.fit_transform(subset[["Suitability Score"]])
+    #subset["Suitability Score"] = scaler.fit_transform(subset[["Suitability Score"]])
     
     #rank the counties
     if high: #sort the counties in descending order based on the number of counties requested
