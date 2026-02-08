711cb9b1b51b29a41ab39f6391fc7d028ce5b841
Sahana Krishnaraj <143361100+sahana-krishnaraj@users.noreply.github.com>
2026-02-07 22:00:12 -0500

    Added Pareto to map

--- app.py
+++ app.py
@@ -11,29 +11,35 @@ from streamlit_folium import st_folium
 @st.cache_data
 def load_data(url):
     map_data = pd.read_csv(url)
-    #map_data["Drought Risk Score"] = 0.0*map_data["None"] + 0.1*map_data["D0"] + 0.3*map_data["D1"] + 0.4*map_data["D2"] + 0.6*map_data["D3"] + 0.9*map_data["D4"]
     map_data["geometry"] = map_data["geometry"].apply(wkt.loads) 
     return gpd.GeoDataFrame(map_data, crs="EPSG:4326")
 
-map_data = load_data("condensed.csv")    
+map_data = load_data("true_final.csv")    
 
 
-def environmental_score(gdf, wildfire_user, drought_user, rank):
+def environmental_score(gdf, wildfire_user, drought_user, wind_user, oil_user, gas_user, rank):
     #make a copy
     gdf = gdf.copy()
+    gdf = gdf[gdf["Pareto Efficient"] == True] #filter for pareto efficient locations
 
     #compute a proportion of the weight so it is scalable
     total_weight = wildfire_user + drought_user
     wildfire_weight = wildfire_user / total_weight
     drought_weight = drought_user / total_weight
+    wind_weight = wind_user / total_weight
+    oil_weight = oil_user / total_weight
+    gas_weight = gas_user / total_weight
 
     #compute the risk score
-    gdf["Suitability Score"] = (wildfire_weight * gdf["Wildfire Hazard Potential"] +
-                                   drought_weight * gdf["Drought Risk Score"])
+    gdf["Suitability Score"] = (wildfire_weight * gdf["Wildfire Hazard Potential Score"] +
+                                   drought_weight * gdf["Drought Risk Score"] 
+                                    + wind_weight * gdf["Wind Plant Capacity"]
+                                   - oil_weight * gdf["Oil Production Quantity"]
+                                   - gas_weight * gdf["Gas Production Quantity"])
     
     #rank the counties from the highest to lowest score
     top_gdf = gdf.sort_values(by="Suitability Score", ascending=False).head(rank) #will be fed into the map
-    rankings = gdf[["County","State","Suitability Score"]].sort_values(by="Suitability Score", ascending=False).head(rank) #used to display
+    rankings = gdf[["County","State Name","Suitability Score"]].sort_values(by="Suitability Score", ascending=False).head(rank) #used to display
 
     rankings = rankings.reset_index(drop=True)
     rankings.index = rankings.index + 1 
@@ -77,29 +83,14 @@ with col1:
     st.markdown(f"#### Slider Options")
     WFH_value = st.slider("Wildfire Weight", 0.0, 1.0, 0.5)
     DR_value = st.slider("Drought Weight", 0.0, 1.0, 0.5)
+    WI_value = st.slider("Wind Energy Weight", 0.0, 1.0, 0.5)
+    OI_value = st.slider("Oil Production Weight", 0.0, 1.0, 0.5)
+    GA_value = st.slider("Gas Production Weight", 0.0, 1.0, 0.5)
     rank_value = st.slider("Number of Locations", 1, 20, 5)
-    rankings, top_gdf = environmental_score(map_data, WFH_value, DR_value, rank_value)
+    rankings, top_gdf = environmental_score(map_data, WFH_value, DR_value, WI_value, OI_value, GA_value, rank_value)
     st.dataframe(rankings)
 
 
 with col2:
     st.markdown(f"#### Top {rank_value} Recommended Counties:")
     st_folium(build_map(top_gdf), width = "stretch")
-
-
-"""
-#user input
-st.text("Change the slides to select weights for the map")
-WFH_value = st.slider("Select a weight for Wildfire Hazard Potential", 0.0, 1.0, 0.5)
-DR_value = st.slider("Select a weight for Drought Risk", 0.0, 1.0, 0.5)
-rank_value = st.slider("Select number of locations to recommend", 1, 20, 5)
-
-#call the function
-rankings, top_gdf = environmental_score(map_data, WFH_value, DR_value, rank_value)
-
-st.header("Top " + str(rank_value) + " Recommended Counties:")
-
-#display the table and map
-st.dataframe(rankings)
-st_folium(build_map(top_gdf), width=True)
-"""
