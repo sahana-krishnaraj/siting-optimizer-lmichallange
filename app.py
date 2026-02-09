caf88216daa48661c3cb714b94b9ed1b1913c965
Sahana Krishnaraj <143361100+sahana-krishnaraj@users.noreply.github.com>
2026-02-08 22:36:39 -0500

    major update

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def load_data(url):
     map_data["geometry"] = map_data["geometry"].apply(wkt.loads) 
     return gpd.GeoDataFrame(map_data, crs="EPSG:4326")
 
-map_data = load_data("dataset.csv") #load the dataset
+map_data = load_data("changed_data.csv") #load the dataset
 
 
 def environmental_score(gdf, wildfire_user, drought_user, wind_user, oil_user, gas_user, rank):
@@ -24,15 +24,26 @@ def environmental_score(gdf, wildfire_user, drought_user, wind_user, oil_user, g
     gdf = gdf[gdf["Pareto Efficient"] == True] #filter for pareto efficient locations
 
     ##higher weight for wildfire, drought, oil, gas is less suitable and higher for wind is more suitable
-    total_weight = wildfire_user + drought_user + wind_user + oil_user + gas_user
-    wildfire_weight = wildfire_user / total_weight
-    drought_weight = drought_user / total_weight
-    wind_weight = wind_user / total_weight
-    oil_weight = oil_user / total_weight
-    gas_weight = gas_user / total_weight
+    total_weight = abs(wildfire_user) + abs(drought_user) + abs(wind_user) + abs(oil_user) + abs(gas_user)
+
+    if total_weight == 0:
+        st.warning("All weights are zero. Defaulting to equal importance for all factors.")
+        wildfire_weight = drought_weight = wind_weight = oil_weight = gas_weight = 0.2
+    else:
+        wildfire_weight = wildfire_user / total_weight
+        drought_weight = drought_user / total_weight
+        wind_weight = wind_user / total_weight
+        oil_weight = oil_user / total_weight
+        gas_weight = gas_user / total_weight
 
     #compute the risk score
-    gdf["Environmental Compatibility (%)"] = ((wind_weight * gdf["Wind Plant Capacity"]) + (wildfire_weight * gdf["Wildfire Hazard Potential Score"] + drought_weight * gdf["Drought Risk Score"]) + oil_weight * gdf["Oil Production Quantity"] + gas_weight * gdf["Gas Production Quantity"]) 
+    gdf["Environmental Compatibility (%)"] = (
+    wind_weight * gdf["Wind Plant Capacity"]
+    - wildfire_weight * gdf["Wildfire Hazard Potential Score"]
+    - drought_weight * gdf["Drought Risk Score"]
+    + oil_weight * gdf["Oil Production Quantity"]
+    + gas_weight * gdf["Gas Production Quantity"]
+) 
 
     scaler = MinMaxScaler(feature_range=(0, 100))
     gdf["Environmental Compatibility (%)"] = scaler.fit_transform(gdf[["Environmental Compatibility (%)"]])
@@ -67,7 +78,6 @@ def build_map(gdf):
             'fillOpacity': 0.7,
         },
         ).add_to(m)
-    
     return m
 
 #STREAMLIT
@@ -85,16 +95,16 @@ with col1:
     #WILDFIRE
     st.write("Wildfire Weight")
     subcol1, subcol2, subcol3 = st.columns([0.25, 0.3, 0.25])
-    subcol1.markdown("<span style='font-size: 12px;'>Low Priority</span>", unsafe_allow_html=True)
+    subcol1.markdown("<span style='font-size: 12px;'>Low Wildfire Risk</span>", unsafe_allow_html=True)
     WFH_value = subcol2.slider("", 0.0, 1.0, 0.5, help="Allows you to adjust the importance wildfire risk", label_visibility="collapsed")
-    subcol3.markdown("<span style='font-size: 12px;'>High Priority</span>", unsafe_allow_html=True)
+    subcol3.markdown("<span style='font-size: 12px;'>High Wildfire Risk</span>", unsafe_allow_html=True)
 
     #DROUGHT
     st.write("Drought Weight")
     subcol1, subcol2, subcol3 = st.columns([0.25, 0.3, 0.25])
-    subcol1.markdown("<span style='font-size: 12px;'>Low Priority</span>", unsafe_allow_html=True)
+    subcol1.markdown("<span style='font-size: 12px;'>Low Drought Risk</span>", unsafe_allow_html=True)
     DR_value = subcol2.slider("", 0.0, 1.0, 0.5, help="Allows you to adjust the importance drought risk", label_visibility="collapsed")
-    subcol3.markdown("<span style='font-size: 12px;'>High Priority</span>", unsafe_allow_html=True)
+    subcol3.markdown("<span style='font-size: 12px;'>High Drought Risk</span>", unsafe_allow_html=True)
 
     #WIND ENERGY
     st.write("Wind Energy Weight")
@@ -106,18 +116,18 @@ with col1:
     #OIL PRODUCTION
     st.write("Oil Production Weight")
     subcol1, subcol2, subcol3 = st.columns([0.25, 0.3, 0.25])
-    subcol1.markdown("<span style='font-size: 12px;'>Low Sustainability</span>", unsafe_allow_html=True)
+    subcol1.markdown("<span style='font-size: 12px;'>Low Preference</span>", unsafe_allow_html=True)
     OI_value = subcol2.slider("", -1.0, 1.0, 0.0, help="Allows you to adjust the importance oil production", label_visibility="collapsed")
-    subcol3.markdown("<span style='font-size: 12px;'>High Sustainability</span>", unsafe_allow_html=True)
+    subcol3.markdown("<span style='font-size: 12px;'>High Preference</span>", unsafe_allow_html=True)
 
     #GAS PRODUCTION
     st.write("Gas Production Weight")
     subcol1, subcol2, subcol3 = st.columns([0.25, 0.3, 0.25])
-    subcol1.markdown("<span style='font-size: 12px;'>Low Sustainability</span>", unsafe_allow_html=True)
+    subcol1.markdown("<span style='font-size: 12px;'>Low Preference</span>", unsafe_allow_html=True)
     GA_value = subcol2.slider("", -1.0, 1.0, 0.0, help="Allows you to adjust the importance gas production", label_visibility="collapsed")
-    subcol3.markdown("<span style='font-size: 12px;'>High Sustainability</span>", unsafe_allow_html=True)
+    subcol3.markdown("<span style='font-size: 12px;'>High Preference</span>", unsafe_allow_html=True)
 
-    rank_value = st.slider("Number of Locations", 1, 38, 3)
+    rank_value = st.slider("Number of Locations", 1, 10, 3)
     rankings, top_gdf = environmental_score(map_data, WFH_value, DR_value, WI_value, OI_value, GA_value, rank_value)
     st.dataframe(rankings)
 
